b7a249a4f4fe6f34ff1448dcc72c3f8b1581ba0f
taras1313 <38490294+taras1313@users.noreply.github.com>
2018-05-04 16:14:17 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,33 +1,16 @@
 
-alert('Введіть будь ласка числове значення');
-const fiboNubmer = parseFloat(prompt('Введіть порядковий номер числа Фібоначчі, яке ви хочете отримати'));
+let fiboNubmer;
 let fiboValue = [];
 fiboValue[0] = 1;
 fiboValue[1] = 1;
+let i = 2;
 
+do {
+    alert('введіть будь ласка числове значення');
+    fiboNubmer = parseFloat(prompt('Введіть порядковий номер числа Фібоначчі, яке ви хочете отримати'));
+} while (isNaN(fiboNubmer)); 
 for (let i = 2; i < fiboNubmer; i++) {
     fiboValue[i]=fiboValue[i-1]+fiboValue[i-2];
 }
-
 document.write(fiboValue.slice(-1));
-
-
-
-// let fiboNubmer;
-// let fiboValue = [];
-// fiboValue[0] = 1;
-// fiboValue[1] = 1;
-// let i;
-
-// do {
-//     alert('введіть будь ласка числове значення');
-//     fiboNubmer = parseFloat(prompt('Введіть порядковий номер числа Фібоначчі, яке ви хочете отримати'));
-//     i = 2;
-//     i++;
-//     fiboValue[i]=fiboValue[i-1]+fiboValue[i-2];
-// } while ((isNaN(fiboNubmer)) && (fiboNubmer>i)); 
-
-// document.write(fiboValue.slice(-1));
-
-
 
