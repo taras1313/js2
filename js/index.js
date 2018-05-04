2f308d4a00df20aebf71bfcc019f09cecefdceae
Taras Yakymyuk <jakimuk.taras.semenowuch@gmail.com>
2018-05-04 12:27:45 +0000

    js fib

--- js/index.js
+++ js/index.js
@@ -1,107 +1,33 @@
-// for (let i = 100; i >= 0; i-- ) {
-//     if (i % 1 === 0){
-//      document.write(i+ '<br>');   
-//     }
-    
-    
-// }        
 
-// i = 0;
-// while (i <= 100) {
-//     document.write(i + "<br>");
-//     i++;
-    
-// }
-// let j = 0
-// do {
-//     document.write(j + "<br>");
-//     j++;
+alert('Введіть будь ласка числове значення');
+const fiboNubmer = parseFloat(prompt('Введіть порядковий номер числа Фібоначчі, яке ви хочете отримати'));
+let fiboValue = [];
+fiboValue[0] = 1;
+fiboValue[1] = 1;
 
-// } while (j <= 100) 
-// let a;    
-// do {
-//     alert('Please enter a number !');
-//      a = parseFloat(prompt('Enter A'));
-// } while (isNaN (a));
-
-// document.write(a);
-
-
-// let a; 
-
-
-// do {
-//     alert('please enter number');
-//     a = parseFloat(prompt('Enter coef A'));
-    
-// } while (isNaN(a));
-
-// document.write(a + 1);
-
-// function prn(value) {
-//     document.write(value + '<br>')
-// }
-
-// let products= ['tesla model x', 'Porshe Panamera', 'McLaren F1']
-// // products.push('ferrari laferrari');
-// products[3] = "bugatti veyron";
-// products[4] = 'something';
-// // prn(products.length); 
-// for (let i = 0; i < products.length; i++) {
-//     if (products[i] === undefined) {
-//         continue;
-//     }
-    
-//     prn(products[i]);
-// }
-
-// products.forEach(prn);
-// products.forEach(function(element){
-//     document.write(element+'<br>');
-// });
-
-// const printVal = (el => document.write(el +'<br>'));
-// products.forEach(el => document.write(el +'<br>')); 
-
-// while(products.length > 0) {
-//     prn(products.pop())
-    
-// }
-
-// products.sort((a,b)=> a- b );
-// prn(products)
-
-// let begin = alert('Begin?');
-// let question = prompt("Whats off name of JS?");
-
-// if (question == "EcmaScript") {
-//     alert('that was good');
-// }
-// else {
-//     alert('how about ecmascript?')
-// }
-
-
-const n = parseFloat(prompt('Введіть порядковий номер числа Фібоначчі, яке ви хочете отримати'));
-let fibo = [];
-fibo[0] = 1;
-fibo[1] = 1;
-for (let i = 2; i < n; i++) {
-    fibo[i]=fibo[i-1]+fibo[i-2];
+for (let i = 2; i < fiboNubmer; i++) {
+    fiboValue[i]=fiboValue[i-1]+fiboValue[i-2];
 }
-alert( fibo.slice(-1) );
+
+document.write(fiboValue.slice(-1));
 
 
-// let n;
-// const fibo = [0,1];
-// let i = 2;
+
+// let fiboNubmer;
+// let fiboValue = [];
+// fiboValue[0] = 1;
+// fiboValue[1] = 1;
+// let i;
+
 // do {
-//     alert('Вводьте лише числа');
-//     n = parseFloat(prompt('Введіть порядковий номер числа Фібоначчі, яке ви хочете отримати'));
-//     let i = 2;
+//     alert('введіть будь ласка числове значення');
+//     fiboNubmer = parseFloat(prompt('Введіть порядковий номер числа Фібоначчі, яке ви хочете отримати'));
+//     i = 2;
 //     i++;
-//     fibo[i]=fibo[i-1]+fibo[i-2];
+//     fiboValue[i]=fiboValue[i-1]+fiboValue[i-2];
+// } while ((isNaN(fiboNubmer)) && (fiboNubmer>i)); 
 
-// } while(isNaN(n)) (i < 2);
+// document.write(fibo.slice(-1));
 
-// alert(fibo.slice(-1));
+
+
